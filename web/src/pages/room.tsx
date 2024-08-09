1d977da036f68253b6664632741a61c2ebfe0371
Rafael Cruz <rcas2009@outlook.com>
2024-08-09 20:41:54 -0300

    Função create room

--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router-dom";
-import amaLogo from "../assets/ama-logo.svg";
 import { ArrowRight, Share2 } from "lucide-react";
 import { toast } from "sonner";
-import { Message } from "../components/message";
+import amaLogo from "../assets/ama-logo.svg";
+import { Messages } from "../components/messages";
+import { Suspense } from "react";
 
 export function Room() {
   const { roomID } = useParams();
@@ -14,34 +15,37 @@ export function Room() {
       navigator.share({ url });
     } else {
       navigator.clipboard.writeText(url);
-
-      toast.info("The room URL was copied to your clipboard!");
+      toast.info("URL da sala copiada para área de transferência!");
     }
+  }
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    // Handle form submission logic here
   }
 
   return (
     <div className="mx-auto max-w-[640px] flex flex-col gap-6 py-10 px-4">
       <div className="flex items-center gap-3 px-3">
         <img src={amaLogo} alt="AMA" className="h-5" />
-
         <span className="text-sm text-zinc-500 truncate">
-          {" "}
           Código da sala: <span className="text-zinc-300">{roomID}</span>
         </span>
-
         <button
-          type="submit"
+          type="button"
           onClick={handleShareRoom}
           className="ml-auto bg-zinc-800 text-zinc-300 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-zinc-700"
+          aria-label="Compartilhar URL da sala"
         >
           Compartilhar
           <Share2 className="size-4" />
         </button>
       </div>
-
       <div className="h-px w-full bg-zinc-900"></div>
-
-      <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offeset-2 ring-offset-zinc-950 focus-within:ring-1">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1"
+      >
         <input
           type="text"
           name="theme"
@@ -57,16 +61,9 @@ export function Room() {
           <ArrowRight className="size-4" />
         </button>
       </form>
-
-      <ol className="list-decimal list-outside px-3 space-y-8">
-        <Message
-          text="Texto de pergunta teste 1"
-          amountOfReactions={10}
-          answered
-        />
-        <Message text="Teste 2" amountOfReactions={12} />
-        <Message text="AGAWADWADWD" amountOfReactions={15} />
-      </ol>
+      <Suspense fallback={<p>Carregando mensagens...</p>}>
+        <Messages />
+      </Suspense>
     </div>
   );
 }
