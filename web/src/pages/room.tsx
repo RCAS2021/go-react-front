169f77c7f6032466f98b775b54c38f4f01b93d85
Rafael Cruz <rcas2009@outlook.com>
2024-08-09 20:51:17 -0300

    Função criar perguntas

--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom";
-import { ArrowRight, Share2 } from "lucide-react";
 import { toast } from "sonner";
-import amaLogo from "../assets/ama-logo.svg";
 import { Messages } from "../components/messages";
 import { Suspense } from "react";
+import { CreateMessageForm } from "../components/create-message-form";
+import { Share2 } from "lucide-react";
+
+import amaLogo from "../assets/ama-logo.svg";
 
 export function Room() {
   const { roomID } = useParams();
@@ -17,11 +19,6 @@ export function Room() {
       navigator.clipboard.writeText(url);
       toast.info("URL da sala copiada para área de transferência!");
     }
-  }
-
-  function handleSubmit(event: React.FormEvent) {
-    event.preventDefault();
-    // Handle form submission logic here
   }
 
   return (
@@ -42,25 +39,7 @@ export function Room() {
         </button>
       </div>
       <div className="h-px w-full bg-zinc-900"></div>
-      <form
-        onSubmit={handleSubmit}
-        className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1"
-      >
-        <input
-          type="text"
-          name="theme"
-          placeholder="Qual a sua pergunta?"
-          autoComplete="off"
-          className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
-        />
-        <button
-          type="submit"
-          className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
-        >
-          Criar pergunta
-          <ArrowRight className="size-4" />
-        </button>
-      </form>
+      <CreateMessageForm></CreateMessageForm>
       <Suspense fallback={<p>Carregando mensagens...</p>}>
         <Messages />
       </Suspense>
